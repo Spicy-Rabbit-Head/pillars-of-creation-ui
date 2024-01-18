7dbbed85dfd24386c808a4b25baaf5d9377b1119
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-01-18 22:39:06 +0800

    更新

--- style/animation.ts
+++ style/animation.ts
@@ -1,20 +1,25 @@
-export default {
+export const animation = {
   /**
    * 关键帧
    */
   keyframes: {
-    ping: '{0% {box-shadow: 0 0 1px 0; opacity: 1;} 100% {box-shadow: 0 0 1px 6px; opacity: 0;}}'
+    'button-ping':
+      '{0% {box-shadow: 0 0 1px 0 var(--poc-button-border-color); opacity: 1;} 100% {box-shadow: 0 0 1px 6px var(--poc-button-border-color); opacity: 0;}}'
   },
+  /**
+   * 动画属性
+   */
+  property: {},
   /**
    * 时间函数
    */
   timingFns: {
-    ping: 'cubic-bezier(0, 0, 0.2, 1)'
+    'button-ping': 'cubic-bezier(0, 0, 0.2, 1)'
   },
   /**
    * 动画次数
    */
   counts: {
-    ping: 'infinite'
+    'button-ping': 1
   }
 }
