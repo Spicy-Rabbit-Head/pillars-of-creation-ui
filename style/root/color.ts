187c4c2b3f7c24a5378c72c1b41961cf7bb1461e
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-01-20 20:40:42 +0800

    更新

---
+++ style/root/color.ts
@@ -0,0 +1,12 @@
+/**
+ * 基本颜色
+ */
+export const baseColor = {
+  '--poc-color-default': '192,196,204',
+  '--poc-color-primary': '64,158,255',
+  '--poc-color-success': '103,194,58',
+  '--poc-color-warning': '230,162,60',
+  '--poc-color-info': '32,201,151',
+  '--poc-color-error': '245,108,108',
+  '--poc-color-disabled': '192,196,204'
+}
