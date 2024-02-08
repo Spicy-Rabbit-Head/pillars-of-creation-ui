57eefd303bddfb9b9768854f964d9d285a789f6c
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-02-08 19:18:52 +0800

    更新

---
+++ style/components/badge.ts
@@ -0,0 +1,25 @@
+import type { CSSVariables, ComponentRoleClass } from '../symbol'
+
+/**
+ * 徽章变量
+ */
+export const badgeVariables: CSSVariables = {
+  '--poc-badge-color': 'var(--poc-text-light-color)',
+  '--poc-badge-bg-color': 'rgb(var(--poc-color-error))',
+  '--poc-badge-b-color': 'var(--poc-color-error-opacity-8)'
+}
+
+/**
+ * 徽章样式
+ */
+export const badgeStyle: CSSVariables = {
+  color: 'var(--poc-badge-color)',
+  backgroundColor: 'var(--poc-badge-bg-color)',
+  boxShadow: 'var(--poc-shadow-border) var(--poc-badge-b-color)'
+}
+
+export const badge: ComponentRoleClass = {
+  name: 'badge',
+  variables: badgeVariables,
+  styles: badgeStyle
+}
