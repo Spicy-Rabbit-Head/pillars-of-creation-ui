348a4d12b5cbad23060a51158c66ab894e4241bc
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-02-10 18:12:31 +0800

    更新

---
+++ style/components/button.ts
@@ -0,0 +1,40 @@
+import type { CSSPseudoClass, CSSVariables, ComponentRoleClass } from '../symbol'
+
+/**
+ * 按钮变量
+ */
+export const buttonVariables: CSSVariables = {
+  '--poc-button-color': 'var(--poc-text-dark-color)',
+  '--poc-button-bg-color': 'transparent',
+  '--poc-button-hover-color': 'var(--poc-button-color)',
+  '--poc-button-border-color': 'rgb(var(--poc-color-default))',
+  '--poc-button-hover-bg-color': 'var(--poc-color-default-opacity-5)'
+}
+
+/**
+ * 按钮样式
+ */
+export const buttonStyle: CSSVariables = {
+  color: 'var(--poc-button-color)',
+  fontSize: 'var(--poc-font-size)',
+  backgroundColor: 'var(--poc-button-bg-color)',
+  border: 'var(--poc-border-width) var(--poc-border-style) var(--poc-button-border-color)',
+  transition: 'var(--poc-transition-color), var(--poc-transition-background)'
+}
+
+export const buttonPseudoClass: CSSPseudoClass[] = [
+  {
+    key: 'hover',
+    value: {
+      backgroundColor: 'var(--poc-button-hover-bg-color)',
+      color: 'var(--poc-button-hover-color)'
+    }
+  }
+]
+
+export const button: ComponentRoleClass = {
+  name: 'button',
+  variables: buttonVariables,
+  styles: buttonStyle,
+  pseudoClass: buttonPseudoClass
+}
