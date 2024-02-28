15f712645ada2b88ed8ab964ca7d9a8261639151
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-02-28 18:46:30 +0800

    更新

--- style/root/backgrounds.ts
+++ style/root/backgrounds.ts
@@ -2,7 +2,7 @@ import { typeClass } from '../symbol'
 
 import type { Rule } from 'unocss'
 
-export const backgrounds = [
+export const backgroundsClass = [
   ...typeClass().reduce((acc, type) => {
     acc.push([`poc-bg-${type}`, { background: `rgb(var(--poc-color-${type}))` }])
     return acc as Rule[]
