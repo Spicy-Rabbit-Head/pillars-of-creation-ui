d81e89dd136755626e173bb6e86ce877d6c1ee32
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-03-01 17:01:23 +0800

    更新

---
+++ style/root/animation/animation.ts
@@ -0,0 +1,26 @@
+import type { AnimationClass } from '../../symbol'
+
+const shadowPing: AnimationClass = {
+  key: 'shadow-ping',
+  keyframes: [
+    {
+      frames: '0%',
+      values: {
+        'box-shadow': '0 0 1px 0 var(--poc-shadow-color)',
+        opacity: 1
+      }
+    },
+    {
+      frames: '100%',
+      values: {
+        'box-shadow': '0 0 1px 6px var(--poc-shadow-color)',
+        opacity: 0
+      }
+    }
+  ],
+  duration: '1s',
+  timingFns: 'cubic-bezier(0, 0, 0.2, 1)',
+  counts: 1
+}
+
+export const animation: AnimationClass[] = [shadowPing]
