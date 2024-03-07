627604c9267bcf7d9feae3b3bfff1555acc06ac3
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-03-07 18:32:15 +0800

    更新

---
+++ demos/collapse/alone.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <poc-collapse-panel title="面板一" arrow-type="left">
+    <p>面板一内容</p>
+    <p>面板一内容</p>
+  </poc-collapse-panel>
+  <br />
+  <poc-collapse-panel card title="面板二" icon="i-ep-aim">
+    <p>面板二内容</p>
+  </poc-collapse-panel>
+</template>
+
+<style scoped></style>
