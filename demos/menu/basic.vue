0a1e99765f1f98d42dba5e99b282a784227bf40a
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-03-10 22:23:40 +0800

    更新

---
+++ demos/menu/basic.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <poc-menu style="max-width: 240px">
+    <poc-menu-group label="1">
+      <poc-menu-item label="1-1">
+        Menu Item 1
+      </poc-menu-item>
+      <poc-menu-item label="1-2">
+        Menu Item 2
+      </poc-menu-item>
+    </poc-menu-group>
+    <poc-menu-item label="2">
+      Menu Item 3
+    </poc-menu-item>
+  </poc-menu>
+</template>
+
+<style scoped></style>
