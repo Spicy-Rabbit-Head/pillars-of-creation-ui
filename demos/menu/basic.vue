d6ab959167b69f4eaa6e89d53d958bc02360a76e
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-03-12 20:14:38 +0800

    更新

--- demos/menu/basic.vue
+++ demos/menu/basic.vue
@@ -2,16 +2,25 @@
 
 <template>
   <poc-menu style="max-width: 240px">
-    <poc-menu-group label="1">
-      <poc-menu-item label="1-1">
-        Menu Item 1
-      </poc-menu-item>
-      <poc-menu-item label="1-2">
-        Menu Item 2
-      </poc-menu-item>
-    </poc-menu-group>
-    <poc-menu-item label="2">
-      Menu Item 3
+    <poc-menu-item label="1" icon="i-ep-camera">
+      菜单一
+      <template #group>
+        <poc-menu-item label="1-1">
+          子菜单一
+        </poc-menu-item>
+        <poc-menu-item label="1-2">
+          子菜单二
+        </poc-menu-item>
+      </template>
+    </poc-menu-item>
+    <poc-menu-item label="2" icon="i-ep-dish">
+      菜单二
+    </poc-menu-item>
+    <poc-menu-item label="3" icon="i-ep-basketball" disabled>
+      菜单三
+    </poc-menu-item>
+    <poc-menu-item label="4" icon="i-ep-avatar">
+      菜单四
     </poc-menu-item>
   </poc-menu>
 </template>
