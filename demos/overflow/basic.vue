df84b87e9c51b9b387d0819c52f02a844695c815
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-03-13 20:38:56 +0800

    更新

---
+++ demos/overflow/basic.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const items = Array.from({ length: 15 }, (_, index) => ({ index: index + 1 }))
+</script>
+
+<template>
+  <div class="max-w-80 border border-solid p-2.5">
+    <poc-overflow :items="items">
+      <template #default="{ item }">
+        <div class="item">
+          元素 {{ item.index }}
+        </div>
+      </template>
+      <template #counter="{ count }">
+        <div class="item">
+          {{ `+${count}` }}
+        </div>
+      </template>
+    </poc-overflow>
+  </div>
+</template>
+
+<style scoped>
+.item {
+  padding: 4px 10px;
+  margin-inline-end: 4px;
+  white-space: nowrap;
+  border: #7db1e0 1px solid;
+  border-radius: 4px;
+}
+</style>
