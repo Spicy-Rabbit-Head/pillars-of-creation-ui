9276b04023294795c9be9bd959d26a3a4366b18a
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-03-14 20:53:29 +0800

    更新

--- demos/badge/basic.vue
+++ demos/badge/basic.vue
@@ -8,7 +8,12 @@
     <poc-badge :content="34" type="warning"></poc-badge>
     <poc-badge :content="34" type="info"></poc-badge>
     <poc-badge :content="34" type="disabled"></poc-badge>
+    <div style="width: 10px; height: 10px; background: var(--color)" class="aaa"></div>
   </poc-space>
 </template>
 
-<style scoped></style>
+<style scoped>
+.aaa {
+  --color: #41d1ff;
+}
+</style>
