19462d7bbc0586652f941d34a4277c8fb787c084
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-03-16 20:08:27 +0800

    更新

---
+++ demos/tooltip/basic.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <poc-tooltip>
+    <template #trigger>
+      <poc-button type="success">
+        到我这里来
+      </poc-button>
+    </template>
+    气泡提示
+  </poc-tooltip>
+</template>
+
+<style scoped></style>
