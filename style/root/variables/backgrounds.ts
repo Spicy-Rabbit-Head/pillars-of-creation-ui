cca47bd17c38c455eac721392919508760f6dfde
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-03-19 20:05:36 +0800

    更新

---
+++ style/root/variables/backgrounds.ts
@@ -0,0 +1,13 @@
+import type { CSSVariables } from '../../symbol'
+
+export const backgrounds: CSSVariables = {
+  '--poc-bg-color-base': '#fff',
+  '--poc-bg-color-reverse': '#131719',
+  '--poc-fill-color-base': '#fff',
+  '--poc-fill-color-secondary': '#d9dfe2',
+  '--poc-fill-color-disabled': '#dee2e6',
+  '--poc-fill-color-hover': '#e9ecef',
+  '--poc-fill-color-humble': '#f3f4f6',
+  '--poc-fill-color-background': '#f8f9fa',
+  '--poc-fill-color-reverse': '#131719'
+}
