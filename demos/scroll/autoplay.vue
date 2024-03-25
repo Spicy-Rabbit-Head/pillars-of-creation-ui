688651e34717d84122b244472d1137785ce43df6
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-03-25 15:49:26 +0800

    更新

---
+++ demos/scroll/autoplay.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div style="max-width: 360px; border: #bd34fe 1px solid">
+    <poc-scroll
+      :height="120"
+      autoplay
+      use-y-bar
+      style="max-width: 360px"
+    >
+      <p v-for="n in 9" :key="n" style="padding: 5px 10px">
+        {{ n }}、在此处滚动鼠标滚轮
+      </p>
+    </poc-scroll>
+  </div>
+</template>
+
+<style scoped></style>
