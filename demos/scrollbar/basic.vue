68a545f0a9310a18635adbe85172abf785525615
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-03-31 18:28:15 +0800

    更新

---
+++ demos/scrollbar/basic.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { boundRange, multipleFixed } from '@/common/utils'
+import { ScrollbarExposed } from '@/components'
+
+import { onMounted, ref } from 'vue'
+
+const pane = ref<HTMLElement>()
+const bar = ref<ScrollbarExposed>()
+const barLength = ref(35)
+
+onMounted(() => {
+  if (!pane.value) return
+
+  barLength.value = boundRange((pane.value.offsetHeight / (pane.value.scrollHeight || 1)) * 100, 5, 99)
+})
+
+function handleScroll() {
+  if (!pane.value || !bar.value) return
+
+  const { scrollTop, scrollHeight, offsetHeight } = pane.value
+
+  if (scrollHeight <= offsetHeight) {
+    bar.value.handleScroll(0)
+    return
+  }
+
+  const percent = multipleFixed(scrollTop / (scrollHeight - offsetHeight || 1), 100, 2)
+  bar.value.handleScroll(percent)
+}
+
+function handleBarScroll(percent: number) {
+  if (!pane.value) return
+
+  const { scrollHeight, offsetHeight } = pane.value
+
+  if (scrollHeight <= offsetHeight) return
+
+  pane.value.scrollTop = (percent * (scrollHeight - offsetHeight)) / 100
+}
+</script>
+
+<template>
+  <div class="wrapper">
+    <div ref="pane" class="scroll-pane" @scroll="handleScroll">
+      <p v-for="n in 20" :key="n">
+        {{ n }}、一段用来滚动的文本
+      </p>
+    </div>
+    <poc-scrollbar ref="bar" :bar-length="barLength" @scroll="handleBarScroll"></poc-scrollbar>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  position: relative;
+  max-width: 500px;
+  padding: 0 10px;
+  border: #bd34fe solid 1px;
+}
+
+.scroll-pane {
+  width: 100%;
+  height: 200px;
+  overflow: auto;
+  scrollbar-width: none;
+}
+
+.scroll-pane::-webkit-scrollbar {
+  display: none;
+}
+</style>
