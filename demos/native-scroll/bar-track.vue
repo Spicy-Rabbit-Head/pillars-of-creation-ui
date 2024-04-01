abf3e1c9b94afd8bb777460b81af58e307ee1520
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-04-01 19:40:58 +0800

    更新

---
+++ demos/native-scroll/bar-track.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div style="max-width: 360px; border: #bd34fe 1px solid">
+    <poc-native-scroll
+      :height="120"
+      use-y-bar
+      use-bar-track
+      :bar-fade="0"
+    >
+      <p v-for="n in 9" :key="n" style="padding: 5px 10px">
+        {{ n }}、在此处滚动鼠标滚轮
+      </p>
+    </poc-native-scroll>
+  </div>
+</template>
+
+<style scoped></style>
