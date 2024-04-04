0740565aeb601308e70d6d923486b8d923cb5a22
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-04-04 21:05:16 +0800

    更新

---
+++ components/avatar/avatar.vue
@@ -0,0 +1,166 @@
+<script setup lang="ts">
+import { ResizeObserver } from '@/components/resize-observer'
+
+import { computed, inject, ref, watch } from 'vue'
+
+import { emitEvent, useNameHelper, useProps } from '@pillars-of-creation-ui/config'
+
+import { avatarProps } from './props.ts'
+import { GROUP_STATE, objectFitValues } from './symbol.ts'
+
+import type { StyleType } from '@pillars-of-creation-ui/config'
+
+defineOptions({ name: 'Avatar' })
+
+const _props = defineProps(avatarProps)
+const props = useProps('avatar', _props, {
+  size: 'default',
+  src: {
+    default: '',
+    static: true
+  },
+  icon: '',
+  circle: false,
+  alt: '',
+  fit: {
+    default: 'cover',
+    validator: value => objectFitValues.includes(value)
+  },
+  srcSet: '',
+  gap: 4,
+  iconScale: 1.4,
+  fallbackSrc: '',
+  color: null,
+  background: null
+})
+
+defineSlots<{
+  default: () => any,
+  icon: () => any
+}>()
+
+const groupState = inject(GROUP_STATE, null)
+
+const nh = useNameHelper('avatar')
+
+const loadFail = ref(false)
+const fallbackFail = ref(false)
+
+const wrapper = ref<HTMLElement>()
+const text = ref<HTMLElement>()
+
+const size = computed(() => {
+  return groupState?.size ?? props.size
+})
+
+const style = computed(() => {
+  const style: StyleType = {
+    [nh.cv('color')]: props.color,
+    [nh.cv('bg-color')]: props.background,
+    [nh.cv('image-fit')]: props.fit
+  }
+
+  if (typeof size.value === 'number') {
+    style[nh.cv('size')] = `${size.value}px`
+  } else {
+    style[nh.cv('size')] = props.size !== 'default' ? (props.size === 'small' ? '26px' : '40px') : null!
+  }
+  style.border = 'var(--poc-avatar-b-width) var(--poc-avatar-b-style) var(--poc-avatar-b-color)'
+
+  return style
+})
+
+watch(
+  () => props.src,
+  () => {
+    loadFail.value = false
+    fallbackFail.value = false
+    scaleText()
+  }
+)
+watch(
+  () => props.fallbackSrc,
+  () => {
+    fallbackFail.value = false
+    scaleText()
+  }
+)
+watch(() => props.gap, scaleText)
+
+defineExpose({
+  loadFail,
+  fallbackFail
+})
+
+function handleError(event: Event) {
+  loadFail.value = true
+  emitEvent(props.onError, event)
+}
+
+let lastText: string | null = null
+
+function scaleText() {
+  const avatarEl = wrapper.value
+  const textEl = text.value
+
+  if (avatarEl && textEl && (lastText === null || lastText !== textEl.textContent)) {
+    lastText = textEl.textContent
+
+    const { offsetWidth: avatarWidth, offsetHeight: avatarHeight } = avatarEl
+    const { offsetWidth: textWidth, offsetHeight: textHeight } = textEl
+    const padding = props.gap * 2
+
+    const ratio = Math.min((avatarWidth - padding) / (textWidth || 1), (avatarHeight - padding) / (textHeight || 1), 1)
+
+    textEl.style.transform = `scale(${ratio})`
+  }
+}
+
+function handleClick(event: MouseEvent) {
+  emitEvent(props.onClick, event)
+}
+</script>
+
+<template>
+  <div
+    ref="wrapper"
+    :class="[
+      'poc-(base-family content-color-base) text-3.5 tabular-nums leading-normal inline-flex shrink-0 items-center justify-center overflow-hidden align-middle select-none rounded',
+      {
+        'rounded-1/2': props.circle
+      }
+    ]"
+    poc-role="avatar"
+    poc-vars="avatar"
+    :style="style"
+    @click="handleClick"
+  >
+    <img
+      v-if="(props.src || props.srcSet) && !loadFail"
+      class="h-full pointer-events-none"
+      style="object-fit: var(--poc-avatar-image-fit)"
+      :src="props.src"
+      :alt="props.alt"
+      :srcset="props.srcSet"
+      @error="handleError"
+    />
+    <img
+      v-else-if="loadFail && props.fallbackSrc && !fallbackFail"
+      class="h-full pointer-events-none"
+      style="object-fit: var(--poc-avatar-image-fit)"
+      :src="props.fallbackSrc"
+      :alt="props.alt"
+      @error="fallbackFail = true"
+    />
+    <template v-else-if="props.icon || $slots.icon">
+      <slot name="icon">
+        <span :class="props.icon" class="inline-flex"></span>
+      </slot>
+    </template>
+    <ResizeObserver v-else :on-resize="scaleText">
+      <span ref="text" class="text-center whitespace-nowrap">
+        <slot></slot>
+      </span>
+    </ResizeObserver>
+  </div>
+</template>
