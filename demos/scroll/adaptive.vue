962f15ab58d78db3590de01982b534cc06fa3b99
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-04-06 20:50:05 +0800

    更新

---
+++ demos/scroll/adaptive.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div style="max-width: 360px; height: 120px; border: #bd34fe 1px solid">
+    <poc-scroll height="100%" use-y-bar style="max-width: 360px">
+      <p v-for="n in 9" :key="n" style="padding: 5px 10px">
+        {{ n }}、在此处滚动鼠标滚轮
+      </p>
+    </poc-scroll>
+  </div>
+</template>
+
+<style scoped></style>
