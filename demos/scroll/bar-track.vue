5dabb8606671658e5a19787c34e468a0adf282ea
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-04-09 21:23:54 +0800

    更新

---
+++ demos/scroll/bar-track.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div style="max-width: 360px; border: #bd34fe 1px solid">
+    <poc-scroll
+      :height="120"
+      use-y-bar
+      use-bar-track
+      :bar-fade="0"
+      style="max-width: 360px"
+    >
+      <p v-for="n in 9" :key="n" style="padding: 5px 10px">
+        {{ n }}、在此处滚动鼠标滚轮
+      </p>
+    </poc-scroll>
+  </div>
+</template>
+
+<style scoped></style>
