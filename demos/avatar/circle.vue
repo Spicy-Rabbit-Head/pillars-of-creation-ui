ba974d87eb059e40b77f3ba72d2ac977b06fd4fb
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-04-11 21:02:22 +0800

    更新

---
+++ demos/avatar/circle.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <poc-avatar circle src="https://cdn.seovx.com/?mom=302" size="small"></poc-avatar>
+  <poc-avatar circle src="https://cdn.seovx.com/?mom=302"></poc-avatar>
+  <poc-avatar circle src="https://cdn.seovx.com/?mom=302" size="large"></poc-avatar>
+  <poc-avatar circle src="https://cdn.seovx.com/?mom=302" :size="50"></poc-avatar>
+</template>
+
+<style scoped></style>
