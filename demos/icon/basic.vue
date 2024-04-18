327b7e315087c06008cb0cc0b4da442c43991128
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-04-18 20:05:59 +0800

    更新

--- demos/icon/basic.vue
+++ demos/icon/basic.vue
@@ -4,6 +4,10 @@
   <poc-icon>
     <span i-ep-apple></span>
   </poc-icon>
+  <poc-icon>
+    <g class="i-ep-failed"></g>
+  </poc-icon>
+  <poc-icon icon="i-ep-avatar"></poc-icon>
 </template>
 
 <style scoped></style>
