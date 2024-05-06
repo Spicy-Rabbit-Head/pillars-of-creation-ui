0f17532de2acb0ba03a00969711e9a60ca1e47c8
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-05-06 21:54:04 +0800

    更新

---
+++ demos/dropdown/basic.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <poc-dropdown>
+    <poc-button type="primary">
+      下拉菜单
+    </poc-button>
+    <template #drop>
+      <poc-dropdown-list>
+        <poc-dropdown-item name="选项一">
+          选项一
+        </poc-dropdown-item>
+        <poc-dropdown-item name="选项二" divided>
+          选项二
+        </poc-dropdown-item>
+        <poc-dropdown-item name="选项三">
+          选项三
+        </poc-dropdown-item>
+      </poc-dropdown-list>
+    </template>
+  </poc-dropdown>
+</template>
+
+<style scoped></style>
