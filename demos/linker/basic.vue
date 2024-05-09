0a78d7e3c8579b32ac0cd07223d41f1f11f8458c
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-05-09 22:23:34 +0800

    更新

---
+++ demos/linker/basic.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <poc-linker>链接</poc-linker>
+  <poc-linker type="primary">
+    链接
+  </poc-linker>
+  <poc-linker type="success">
+    链接
+  </poc-linker>
+  <poc-linker type="error">
+    链接
+  </poc-linker>
+  <poc-linker type="warning">
+    链接
+  </poc-linker>
+  <poc-linker type="info">
+    链接
+  </poc-linker>
+</template>
+
+<style scoped></style>
