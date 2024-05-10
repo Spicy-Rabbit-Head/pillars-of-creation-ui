e6039af4810f90433543a476026d304d39706658
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-05-10 23:21:17 +0800

    更新

---
+++ demos/checkbox/basic.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const checked = ref(false)
+</script>
+
+<template>
+  <poc-checkbox v-model:checked="checked">
+    Checkbox
+  </poc-checkbox>
+  <br />
+  <p style="margin-top: 10px">
+    checked: {{ checked }}
+  </p>
+</template>
+
+<style scoped></style>
