fc4eaf3a003e1572d4d05d6fa5fcad8f60f6712c
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-05-15 20:38:20 +0800

    更新

---
+++ demos/input/basic.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const value = ref('')
+</script>
+
+<template>
+  <poc-input v-model:value="value" placeholder="基础用法" style="max-width: 300px"></poc-input>
+  <p>Input Value: {{ value }}</p>
+</template>
+
+<style scoped></style>
