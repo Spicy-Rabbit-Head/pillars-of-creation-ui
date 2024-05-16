b8d081f8b53f5dc6e1ae7b3066d5690475235ef1
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-05-16 22:37:34 +0800

    更新

---
+++ demos/switch/basic.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const checked = ref(false)
+</script>
+
+<template>
+  <poc-switch v-model:value="checked"></poc-switch>
+  <poc-switch disabled></poc-switch>
+  <br />
+  <p style="margin-top: 10px">
+    checked: {{ checked }}
+  </p>
+</template>
+
+<style scoped></style>
