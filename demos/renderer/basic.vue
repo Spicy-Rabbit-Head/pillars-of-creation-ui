d902feb0ae406e5df896f52f7162dd229a16787e
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-05-17 18:27:38 +0800

    更新

---
+++ demos/renderer/basic.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { h, ref } from 'vue'
+
+const msg = ref('一些内容')
+
+function renderMessage({ message }: { message: string }) {
+  return h('p', [
+    '这是用渲染函数渲染的',
+    h(
+      'span',
+      {
+        style: 'color: red'
+      },
+      [message]
+    )
+  ])
+}
+</script>
+
+<template>
+  <div>
+    <p>这是常规的模版内容</p>
+    <poc-renderer :renderer="renderMessage" :data="{ message: msg }"></poc-renderer>
+  </div>
+</template>
+
+<style scoped></style>
