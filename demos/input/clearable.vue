475467d1cc5e3c5b1773b6c1d22ca962d48c41a7
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-05-18 20:05:56 +0800

    更新

---
+++ demos/input/clearable.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const value = ref('')
+</script>
+
+<template>
+  <poc-input v-model:value="value" clearable style="max-width: 300px"></poc-input>
+</template>
+
+<style scoped></style>
