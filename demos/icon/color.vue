79c5f2e158cec106a0729e437789f8b3f856dd4a
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-05-19 20:15:38 +0800

    更新

---
+++ demos/icon/color.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <poc-icon icon="i-ep-quartz-watch" color="red"></poc-icon>
+  <poc-icon icon="i-ep-quartz-watch" color="orange"></poc-icon>
+  <poc-icon icon="i-ep-quartz-watch" color="green"></poc-icon>
+</template>
+
+<style scoped></style>
