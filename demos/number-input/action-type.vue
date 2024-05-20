6421fef59a253766b47e5f88f7c1eb440b163942
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-05-20 22:34:49 +0800

    更新

---
+++ demos/number-input/action-type.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const value = ref<number>()
+</script>
+
+<template>
+  <poc-space vertical>
+    <poc-number-input max-w-75></poc-number-input>
+    <poc-number-input control-type="left" max-w-75></poc-number-input>
+    <poc-number-input control-type="right-fade" max-w-75></poc-number-input>
+    <poc-number-input control-type="left-fade" max-w-75></poc-number-input>
+    <poc-number-input control-type="none" max-w-75></poc-number-input>
+  </poc-space>
+</template>
