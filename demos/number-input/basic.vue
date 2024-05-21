aef9f00b8b85abb0785bfdae547c82aae8a8fa75
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-05-21 20:04:03 +0800

    更新

---
+++ demos/number-input/basic.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const value = ref<number>()
+</script>
+
+<template>
+  <poc-number-input v-model:value="value" style="max-width: 300px"></poc-number-input>
+  <p>Number Input Value: {{ value }}</p>
+</template>
+
+<style scoped></style>
