2083649a267e0d9020ee75dd3768c22b60cf1fa9
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-05-22 21:52:51 +0800

    更新

---
+++ demos/divider/basic.vue
@@ -0,0 +1,7 @@
+<template>
+  <p>一段文字一段文字一段文字一段文字一段文字</p>
+  <poc-divider></poc-divider>
+  <p>一段文字一段文字一段文字一段文字一段文字</p>
+  <poc-divider>插入文字</poc-divider>
+  <p>一段文字一段文字一段文字一段文字一段文字</p>
+</template>
