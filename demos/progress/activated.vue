dcc20bf512e001483f4ca4dbb4d75d5d17bdbb3c
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-05-23 20:38:22 +0800

    更新

---
+++ demos/progress/activated.vue
@@ -0,0 +1,6 @@
+<template>
+  <div style="max-width: 500px">
+    <poc-progress activated :percentage="68"></poc-progress>
+    <poc-progress activated :stroke-color="['red', 'yellow']" :percentage="68"></poc-progress>
+  </div>
+</template>
