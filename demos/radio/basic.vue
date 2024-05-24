078a8b34ca8150e8bf3d840bfb4544914f544b2c
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-05-24 22:04:06 +0800

    更新

---
+++ demos/radio/basic.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const value = ref('')
+</script>
+
+<template>
+  <poc-radio v-model:value="value" label="radio">
+    Radio
+  </poc-radio>
+  <br />
+  <p style="margin: 10px 0">
+    value: {{ value }}
+  </p>
+  <poc-button type="primary" size="small" @click="value = value ? '' : 'radio'">
+    {{ value ? 'Cancel' : 'Select' }}
+  </poc-button>
+</template>
