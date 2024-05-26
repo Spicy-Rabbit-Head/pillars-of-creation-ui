c299839d9735d139c89030fdb0aec87938469db4
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-05-26 23:01:35 +0800

    更新

---
+++ demos/skeleton/activated.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const activated = ref(true)
+</script>
+
+<template>
+  激活状态：
+  <poc-switch v-model:value="activated"></poc-switch>
+  <br />
+  <br />
+  <poc-skeleton :activated="activated" width="33%" :spread="14"></poc-skeleton>
+  <poc-skeleton :activated="activated" :repeat="4"></poc-skeleton>
+  <poc-skeleton :activated="activated" width="67%"></poc-skeleton>
+</template>
