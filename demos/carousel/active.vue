d5230c3cf37ba6f20cd8afdc27e4eb64ecc0332d
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-05-26 19:09:29 +0800

    更新

---
+++ demos/carousel/active.vue
@@ -0,0 +1,27 @@
+<template>
+  <poc-carousel loop :active-offset="1" style="max-width: 1000px">
+    <poc-carousel-item v-for="n in 5" :key="n">
+      <template #default="{ active }">
+        <div class="demo-pane" :style="{ backgroundColor: active ? 'var(--poc-color-warning-base)' : undefined }">
+          内容{{ n }}
+        </div>
+      </template>
+    </poc-carousel-item>
+  </poc-carousel>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.demo-pane {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 240px;
+  font-size: 30px;
+  color: #fff;
+  background-color: var(--poc-color-primary-base);
+  transition: var(--poc-transition-transform);
+}
+</style>
