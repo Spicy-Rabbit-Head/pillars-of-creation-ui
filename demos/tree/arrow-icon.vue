7afea77c43a4e4b18d7bca137cb4ee8ab937c259
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-05-28 22:11:33 +0800

    更新

---
+++ demos/tree/arrow-icon.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+const data = [
+  {
+    id: 1,
+    label: '动物',
+    parent: 0,
+    expanded: true
+  },
+  {
+    id: 2,
+    label: '鸟类',
+    parent: 5
+  },
+  {
+    id: 3,
+    label: '无脊椎动物',
+    parent: 1
+  },
+  {
+    id: 4,
+    label: '哺乳动物',
+    parent: 5
+  },
+  {
+    id: 5,
+    label: '脊椎动物',
+    parent: 1
+  },
+  {
+    id: 6,
+    label: '喜鹊',
+    parent: 2
+  },
+  {
+    id: 7,
+    label: '蚯蚓',
+    parent: 3
+  },
+  {
+    id: 8,
+    label: '水母',
+    parent: 3
+  },
+  {
+    id: 9,
+    label: '穿山甲',
+    parent: 4
+  },
+  {
+    id: 10,
+    label: '红隼',
+    parent: 2
+  }
+]
+</script>
+
+<template>
+  <poc-tree :data="data" arrow-icon="i-ri-contract-right-line"></poc-tree>
+  <poc-tree :data="data">
+    <template #arrow>
+      <poc-icon icon="i-ri-e-bike-2-line"></poc-icon>
+    </template>
+  </poc-tree>
+</template>
