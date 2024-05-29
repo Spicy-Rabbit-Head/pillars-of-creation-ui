8e2b5cd78e2bb887864f5531372827de0789f753
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-05-29 21:15:28 +0800

    更新

---
+++ demos/color-picker/alpha.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import type { Color } from 'pillars-of-creation-ui'
+
+const color = ref<Color>('rgba(51, 154, 240, 0.6)')
+</script>
+
+<template>
+  <poc-color-picker v-model:value="color" class="ml-25" alpha></poc-color-picker>
+</template>
