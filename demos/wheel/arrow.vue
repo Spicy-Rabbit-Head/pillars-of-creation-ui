5914b3119babb53f696210b4354ce01aaa93dd71
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-06-02 22:26:27 +0800

    更新

---
+++ demos/wheel/arrow.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const options = Array.from({ length: 20 }, (_, index) => `选项${index + 1}`)
+</script>
+
+<template>
+  <poc-wheel arrow :options="options"></poc-wheel>
+</template>
