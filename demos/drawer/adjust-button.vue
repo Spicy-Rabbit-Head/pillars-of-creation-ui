5e8f1faf5991552fa1e65477982565a7734c5886
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2024-06-03 16:54:11 +0800

    更新

---
+++ demos/drawer/adjust-button.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const active = ref(false)
+</script>
+
+<template>
+  <poc-button type="primary" @click="active = !active">
+    打开
+  </poc-button>
+  <poc-drawer
+    v-model:active="active"
+    transfer
+    title="标题"
+    footer
+    confirm-type="success"
+    cancel-type="error"
+    action-size="default"
+  >
+    <p>一些内容</p>
+    <p>一些内容</p>
+    <p>一些内容</p>
+  </poc-drawer>
+</template>
